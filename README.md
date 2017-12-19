246543f0426755d3d4d526a5fcd4ecf2a6972eb7
lo56ve <1046866318@qq.com>
2017-12-19 17:17:58 +0800

    移动端单页面模板

---
+++ README.md
@@ -0,0 +1,13 @@
+### 单页面模板
+
+- postcss
+- autoprefixer
+- px2rem
+- lib-flexible
+- sass
+
+### 特点
+
+- 适用于移动端开发
+- 根据需求自行添加UI框架
+- 改动打包输出，打包之后html与css,js文件夹同级
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <div class="wind-text" @click="fastspeed">
+      <div :class="isFast ? 'fast' : 'slow'">{{text}}</div>
+      <div>{{text.slice(0,16)}}</div>
+      <div>{{text.slice(0,10)}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to template',
+      text: 'XXXXXXXXXXXXXXXXXXXXXXXXX',
+      isFast: false
+    }
+  },
+  methods: {
+    fastspeed () {
+      this.isFast = !this.isFast
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+    li {
+      display: inline-block;
+      margin: 0 10px;
+      a {
+        color: #42b983;
+      }
+    }
+  }
+  .wind-text {
+    div {
+      &:nth-child(2) {
+        transform: rotate(90deg) scale(1.5, 0.5);
+        margin-top: 22%;
+      }
+      &:last-child {
+        transform: translateY(80px);
+      }
+    }
+    .fast {
+      animation: move 0.1s infinite;
+    }
+    .slow {
+      animation: move 0.3s infinite;
+    }
+    @keyframes move {
+      from {
+        transform: rotate(0);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  }
+
+</style>
